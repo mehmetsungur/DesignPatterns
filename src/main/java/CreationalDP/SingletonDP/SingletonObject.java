0953a8ecd06b4d6e5f7253f9989aafe69364752e
mehmetsungur <mehmetsungur90@gmail.com>
2023-08-30 13:37:22 +0300

    update

--- src/main/java/CreationalDP/SingletonDP/SingletonObject.java
+++ src/main/java/CreationalDP/SingletonDP/SingletonObject.java
@@ -3,5 +3,13 @@ package CreationalDP.SingletonDP;
 public class SingletonObject {
     public String message = "Hello";
 
+    private static SingletonObject instance;
 
+    public static SingletonObject getInstance(){
+        if(instance == null){
+            instance = new SingletonObject();
+        }
+
+        return instance;
+    }
 }
