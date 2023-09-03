35d504a24bfe5f6e4fb0873421ea05e9e2ce36dd
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-03 11:49:21 +0300

    update

--- src/main/java/StructuralDP/ProxyDP/ProxyClass.java
+++ src/main/java/StructuralDP/ProxyDP/ProxyClass.java
@@ -1,4 +1,20 @@
 package StructuralDP.ProxyDP;
 
-public class ProxyClass {
+public class ProxyClass implements ImageGenerator {
+    private RealClass realClass;
+
+    public String fullPath;
+
+    public ProxyClass(String fullPath){
+        this.fullPath = fullPath;
+    }
+
+    @Override
+    public void showImage() {
+        if(realClass == null){
+            this.realClass = new RealClass(fullPath);
+        }
+
+        realClass.showImage();
+    }
 }
