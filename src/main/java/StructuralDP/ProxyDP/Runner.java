00c7a94cc729c3f815db73eb5827d5f1a2ecd044
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-03 11:52:00 +0300

    update

--- src/main/java/StructuralDP/ProxyDP/Runner.java
+++ src/main/java/StructuralDP/ProxyDP/Runner.java
@@ -3,5 +3,7 @@ package StructuralDP.ProxyDP;
 public class Runner {
     public static void main(String[] args) {
         RealClass realObject = new RealClass("C:\\image.jpg");
+        System.out.println(realObject.fullPath);
+        System.out.println(realObject.name);
     }
 }
