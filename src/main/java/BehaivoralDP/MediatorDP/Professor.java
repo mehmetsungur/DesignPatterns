c7509584aedf30bfdcaa026cf90e762dc354c7ad
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-09 17:48:40 +0300

    update

--- src/main/java/BehaivoralDP/MediatorDP/Professor.java
+++ src/main/java/BehaivoralDP/MediatorDP/Professor.java
@@ -1,4 +1,13 @@
 package BehaivoralDP.MediatorDP;
 
-public class Professor {
+public class Professor implements Actor {
+    @Override
+    public void receiveMessage(String message) {
+
+    }
+
+    @Override
+    public void sendMessage(String topic, String message) {
+
+    }
 }
