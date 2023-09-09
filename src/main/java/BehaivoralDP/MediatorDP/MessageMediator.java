893ed64e7a2fe27aeb0b3e5f7f3a258aa763b23a
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-09 17:54:10 +0300

    update

--- src/main/java/BehaivoralDP/MediatorDP/MessageMediator.java
+++ src/main/java/BehaivoralDP/MediatorDP/MessageMediator.java
@@ -1,6 +1,14 @@
 package BehaivoralDP.MediatorDP;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MessageMediator implements Mediator {
+    Map<String,Actor> registeredActor = new HashMap<>();
+
+    public void register(String topic, Actor actor){
+        registeredActor.put(topic, actor);
+    }
 
     @Override
     public void dispatch(String topic, String message) {
